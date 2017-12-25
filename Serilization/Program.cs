923e4ecfc0d2f078a84de59de759ba5d57214920
ZhaslanR <zhaslan_12@bk.ru>
2017-12-25 15:56:42 +0600

    Добавьте файлы проекта.

---
+++ Serilization/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace Serilization
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            User[] users = new User[]
+                {
+            new User
+            {
+                ID = 1,
+                person = new Person
+                {
+                    Id = 1,
+                    FullName = "Zhaslan Ryspekov"
+                },
+                Login = "zhaslan",
+                Password = "zhaslan",
+                CreationDate = DateTime.Now,
+                IsDelete = false
+            }
+                };
+
+            //BinaryFormatter serializer = new BinaryFormatter();
+            XmlSerializer serializer = new XmlSerializer(typeof(User[]));
+
+            using (FileStream stream = new FileStream(@"C:\name_of_folder\data.xml", FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(stream, users);
+            }
+
+            using (FileStream stream = new FileStream(@"C:\name_of_folder\data.xml", FileMode.OpenOrCreate))
+            {
+                User[] newUser = serializer.Deserialize(stream) as User[];
+            }
+
+            string json = JsonConvert.SerializeObject(users);
+            User[] newUsers = JsonConvert.DeserializeObject<User[]>(json);
+        }
+    }
+}
---
+++ Serilization/User.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilization
+{
+    [Serializable]
+    public class User
+    {
+        [NonSerialized]
+        [JsonIgnore]
+        private int id; 
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public Person Person { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
